0ab159d8d1bd041c7f82ca2c3053c3a177149c91
Dreamcodedev <demba.dioop@gmail.com>
2021-10-06 13:40:59 +0200

    ajout des filtres

--- project/urls.py
+++ project/urls.py
@@ -3,12 +3,14 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from shop.views import CategoryViewSet, ProductViewSet
+from shop.views import CategoryViewSet, ProductViewSet, ArticleViewSet
 
 router=routers.SimpleRouter()
 
 router.register('category',CategoryViewSet, basename='category')
 router.register('product', ProductViewSet, basename='product')
+router.register('article', ArticleViewSet, basename='article')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
